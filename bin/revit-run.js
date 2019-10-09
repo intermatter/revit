f3c2b792de205a6f912a661bb9aab8bd0cc41343
Waleed Qidan <qidanw@gmail.com>
2019-10-09 08:05:43 +0400

    rev. 18

--- bin/revit-run.js
+++ bin/revit-run.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const shell = require('shelljs');
 var spawn = require('child_process').spawn;
 
@@ -11,38 +12,31 @@ const path = './.revrc';
 
 var revisionNumber;
 
-try
-{
-  if (fs.existsSync(path))
-  {
-    revisionNumber = parseInt(fs.readFileSync("./.revrc", "utf-8").trim());
-  }
-  else
-  {
-    if(!setRevisionNumber)
-      shell.exit(".revrc was found in this directory.\nTo set the current revision and push to master, run: \"revit REVISION\"\nExample: revit 0");
-    else
-      revisionNumber = setRevisionNumber;
-  }
-  revisionNumber++;
-  
-  const addcommit = "git add . && git commit -m \"rev. " + revisionNumber + "\"";
+try {
+    if (fs.existsSync(path)) {
+        revisionNumber = parseInt(fs.readFileSync("./.revrc", "utf-8").trim());
+    } else {
+        if (!setRevisionNumber)
+            shell.exit(".revrc was found in this directory.\nTo set the current revision and proceed with the iteration, run: \"revit REVISION\"\nExample: revit 0");
+        else
+            revisionNumber = setRevisionNumber;
+    }
+    revisionNumber++;
 
-  shell.exec(addcommit, { silent: false })
+    const addcommit = "git add . && git commit -m \"rev. " + revisionNumber + "\"";
 
-  shell.exec("cp " + path + " " + path + ".process", { silent: true });
+    shell.exec(addcommit, { silent: false })
 
-  fs.writeFileSync(path, revisionNumber, {encoding:'utf8', flag:'w'});
+    //fs.writeFileSync(path, revisionNumber, { encoding: 'utf8', flag: 'w' });
+    shell.exec("echo " + revisionNumber + " > " + path, { silent: true });
 
-  spawn('git', ['push', '-u', 'origin', 'master'], {stdio: 'inherit'});
+    spawn('git', ['push', '-u', 'origin', 'master'], { stdio: 'inherit' });
 
-} catch(err) {
-  shell.exec("git reset", { silent: true });
-  shell.exec("rm " + path, { silent: true });
-  shell.exec("mv " + path + ".process " + path, { silent: true });
-  shell.exec("echo Revision was not iterated");
+} catch (err) {
+    shell.exec("git reset", { silent: true });
+
+    shell.exec("echo " + (revisionNumber-1) + " > " + path, { silent: true });
+
+    shell.exec("echo Revision was not iterated");
 }
-
-shell.exec("echo Current revision is '" + revisionNumber +"'");
-
-shell.exec("rm " + path + ".process", { silent: true });
+//shell.exec("echo Current revision is '" + revisionNumber +"'");
