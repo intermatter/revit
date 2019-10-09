5cc28d069008d28ec547e7294ce9fb70ea23ea5b
Waleed Qidan <qidanw@gmail.com>
2019-10-09 04:37:45 +0400

    rev. 3

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Revit
-NPM package to enable revisionary iterations.
 
 This module acts as an alias for "git add . && git commit -m "rev. REVISION_ID" && git push -u origin master", where REVISION_ID is auto-incremented with a local .revrc file.
 
 ## Installation
 ````
-npm -i -g revit
+npm i -g revit
 ````
 
 ## Usage
@@ -13,3 +12,6 @@ To create an iteration, run:
 ````
 revit
 ````
+
+## Note
+.revrc is auto-incremented. It should be committed and should not be manually modified.
--- bin/revit-run.js
+++ bin/revit-run.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
-const shell = require('shelljs')
-shell.exec("rev=$(cat .revrc) && rev=$((rev+1)) && echo \"${rev}\" > .revrc &&  git add . && git commit -m \"rev. ${rev}\" && git push -u origin master")
+import {windows} from 'platform-detect/os.mjs';
+const shell = require('shelljs');
+if(windows)
+shell.exec("cd ..");
+else
+shell.exec("rev=$(cat .revrc) && rev=$((rev+1)) && echo \"${rev}\" > .revrc &&  git add . && git commit -m \"rev. ${rev}\" && git push -u origin master");
